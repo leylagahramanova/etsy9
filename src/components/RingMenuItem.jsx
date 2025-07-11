c97e7977449dcf7f8b08ac88d40f5812da5031fe
leylagahramanova <leylagahramanova13@gmail.com>
2025-07-11 20:18:47 +0400

    ertyui

--- src/components/RingMenuItem.jsx
+++ src/components/RingMenuItem.jsx
@@ -20,29 +20,27 @@ const RingMenuItem = () => {
             key: 'seller-help-text',
             label: (
                 <div className="px-4 py-2 text-black text-sm">
-                   There will be news about our features
+                    There will be news about our features
                 </div>
             ),
             disabled: true,
         },
-      
+
     ];
     return (
         <>
-      <Dropdown trigger={['click']} arrow style={{ padding: '0' }}>
+            <Dropdown trigger={['click']} arrow style={{ padding: '0' }}>
 
-                                    <div className='flex py-1 pr-1 rounded-full hover:bg-blue-100 group cursor-pointer group-hover:text-blue-700'>
-                                        <Tooltip title="Deals" placement="bottom" className='flex'>
-                                            <span className="flex items-center" >
+                <div className='flex py-1 pr-1 rounded-full hover:bg-blue-100 group cursor-pointer group-hover:text-blue-700'>
+                    <Tooltip title="Deals" placement="bottom" className='flex'>
+                        <span className="flex items-center">
+                            <PiBellSimpleRinging size={24} className="text-gray-700 cursor-pointer" />
+                            <RiArrowDownSFill size={20} />
+                        </span>
+                    </Tooltip>
 
-                                                <PiBellSimpleRinging size={24} className="text-gray-700 curso r-pointer" />
-                                                <RiArrowDownSFill size={20} />
-                                            </span>
-
-                                        </Tooltip>
-
-                                    </div>
-                                </Dropdown>
+                </div>
+            </Dropdown>
         </>
     )
 }
